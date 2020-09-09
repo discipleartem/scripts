5e893f8157bebad5a4962a42fcf65e571283b89a
Tomas <discipleartem@gmail.com>
2020-09-09 19:41:10 +0300

    09.2020

---
+++ scale_monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#sudo chmod 755 /$HOME/scale_monitor.sh
+#use standart Ubuntu "Startup Aplication" to add script
+
+# xrandr --output DP-1 --scale 0.75x0.75
+
+xrandr --output DVI-D-0 --scale 0.75x0.75
